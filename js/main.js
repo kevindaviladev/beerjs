f275cf6d35d5529b389ac392da9569ced0049091
Kevin Davila <kdavila@hiper.com.pe>
2022-01-19 23:44:55 -0500

    ready to commit and push v1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//Configurar SW
+let swLocation = "/beerjs/sw.js";
+// "/beerjs/sw.js";
+
+if (navigator.serviceWorker) {
+  if (window.location.href.includes("localhost")) swLocation = "/sw.js"; //Varia según el host
+  navigator.serviceWorker.register(swLocation);
+}
+
+//Logic of web app
+console.log("Hello world!!");
---
+++ sw.js
@@ -0,0 +1,24 @@
+const STATIC_CACHE = "static-v1";
+
+const APP_SHELL = ["/", "index.html", "css/styles.css", "js/main.js"];
+
+self.addEventListener("install", (e) => {
+  const cacheStatic = caches
+    .open(STATIC_CACHE)
+    .then((cache) => cache.addAll(APP_SHELL));
+
+  e.waitUntil(cacheStatic);
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log("fetch! ", e.request);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((res) => {
+        return res || fetch(e.request);
+      })
+      .catch(console.log)
+  );
+  //   e.waitUntil(response);
+});
