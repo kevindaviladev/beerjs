a653a9de3cc1c1ff53654c922191b99c64979ef8
Kevin Davila <kdavila@hiper.com.pe>
2022-01-19 23:56:51 -0500

    fix(manifest-sw): Added logo to app shell cache and changed color of theme in manifest.

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 const STATIC_CACHE = "static-v1";
 
-const APP_SHELL = ["/", "index.html", "css/styles.css", "js/main.js"];
+const APP_SHELL = [
+  "/",
+  "index.html",
+  "css/styles.css",
+  "js/main.js",
+  "images/beerjs-logo.png",
+];
 
 self.addEventListener("install", (e) => {
   const cacheStatic = caches
